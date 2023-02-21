57f8525ffa1efbf9f695145f7bab63f648de52b4
tr1gadaa <118889981+tr1gadaa@users.noreply.github.com>
2023-02-21 17:56:42 +0300

    add scroll

--- test_simple.py
+++ test_simple.py
@@ -7,10 +7,10 @@ from selene.support.shared import browser
 def browser_settings():
     browser.config.browser_name = 'firefox'
     browser.config.hold_browser_open = True
-    browser.config.driver.maximize_window()
 
 def test_positive_case(browser_settings):
     browser.open('https://demoqa.com/automation-practice-form')
+    browser.config.driver.maximize_window()
     browser.element('input[id=firstName]').should(be.blank).type('Mariia')
     browser.element('input[id=lastName]').should(be.blank).type('Levchenko')
     browser.element('input[id=userEmail]').should(be.blank).type('emailfortest@mail.com')
