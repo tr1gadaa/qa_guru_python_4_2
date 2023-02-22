a9ec347a025c04c4a08f333ab3eda0831df806d3
tr1gadaa <118889981+tr1gadaa@users.noreply.github.com>
2023-02-22 13:18:11 +0300

    add tests_google_search.py

--- tests_google_search.py
+++ tests_google_search.py
@@ -17,5 +17,5 @@ def test_google_search_positive(browser_settings):
 
 def test_google_search_negative(browser_settings):
     browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('Selene').press_enter()
-    browser.element('[id="search"]').should(have.no.text('Hello, World'))
+    browser.element('[name="q"]').should(be.blank).type('89912313132121393030303039393').press_enter()
+    browser.element('.card-section').should(have.text('По запросу 89912313132121393030303039393 ничего не найдено.'))
